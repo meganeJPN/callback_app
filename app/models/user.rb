8dce700acacf26616fdb5fcdd5cbafd5254fef6c
meganeJPN <ooosamuuu1985@gmail.com>
2020-08-26 13:03:38 +0900

    Callbackクラス学習用プログラム

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,25 @@
 class User < ApplicationRecord
-  before_create :ensure_has_name
-  # before_create do
+  # before_create :ensure_has_name
+  # # before_create do
+  # #   self.name = 'Taro' if name.blank?
+  # # end
+  # before_save do
+  #   puts '1番目に呼ばれたコールバック'
+  # end
+  # before_save do
+  #   puts '2番目に呼ばれたコールバック'
+  # end
+
+  # after_initialize do |user|
+  #   p user
+  #   puts "オブジェクトを生成しました！"
+  #   user.name = "Taro"
+  # end
+
+  # private
+  # def ensure_has_name
+  #   puts "nameの値をTaroに設定します！"
   #   self.name = 'Taro' if name.blank?
   # end
-  before_save do
-    puts '1番目に呼ばれたコールバック'
-  end
-  before_save do
-    puts '2番目に呼ばれたコールバック'
-  end
-
-  after_initialize do |user|
-    p user
-    puts "オブジェクトを生成しました！"
-    user.name = "Taro"
-  end
-
-  private
-  def ensure_has_name
-    puts "nameの値をTaroに設定します！"
-    self.name = 'Taro' if name.blank?
-  end
+  before_create CreateAgeAtCallback.new
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_024443) do
+ActiveRecord::Schema.define(version: 2020_08_26_025909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
